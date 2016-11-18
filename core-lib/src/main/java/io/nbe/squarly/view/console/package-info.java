602a31952bd3cd266ebed56c073db97353dd5409
Nicolas Beaussart <nic.beaussart@gmail.com>
2016-11-18 17:33:01 +0100

    Cleaning codebase

--- core-lib/src/main/java/io/nbe/squarly/view/console/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The swing based console package
- * @author Nicolas Beaussart
- * @since 16/11/2016
- */
-package io.nbe.engine.view.console;
--- core-lib/src/main/java/io/nbe/squarly/view/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The package where the view is
- * @author Nicolas Beaussart
- * @since 16/11/2016
- */
-package io.nbe.engine.view;
